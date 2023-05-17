febd4db12e4a1e497bb7e54df6ee4cfb7d76c8a5
ChristopherMCDawson <126995245+ChristopherMCDawson@users.noreply.github.com>
2023-05-16 23:34:12 -0400

    All Comments in place, Enjoy ! Christopher Decarie-Dawson

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,19 +1,37 @@
-﻿using ServiceStack;
+﻿/*Name: Christopher Decarie-Dawson
+ *Student#: 040718315
+ CST8359_300
+ Lab1
+ Author: Christopher Decarie-Dawson
+ Release:1.0
+ Date:2023/05/16
+*/
+
+/*
+ The purpose of this program is to take in data  from a word.txt file
+then sort and miluate the data from a menu displayed on the console.
+Doing this within the C# framework , without the (framewroks).
+
+All methods outside of Main have been try/catched with error handling.
+ 
+ */
+
+using ServiceStack;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+// declaring the namespace as lab1
 namespace Lab1
 {
     class Program
     {
      
-
+        // the Main method which holds the menu and the switch cases for the menu options , which in turns calls the other methods.
         static void Main(string[] args)
         {
             bool exit = false;
-
+            // runs till an Exit call
             while (!exit)
             {
                 Console.WriteLine("Menu:");
@@ -29,6 +47,7 @@ namespace Lab1
                 Console.WriteLine("x - Exit");
 
                 Console.Write("Enter your choice: ");
+                // takes user input into the Switch case.
                 string choice = Console.ReadLine();
 
                 switch (choice)
@@ -70,17 +89,17 @@ namespace Lab1
                 Console.WriteLine();
             }
         }
-
+        // This Method is used to Read or import the Words.txt file into the program for use.
         static IList<string> readFile()
         {
             try
-            {
+            { // creates a List and then reads the file and adds the words to the List.
                 IList<string> words;
                 words = System.IO.File.ReadAllLines(@".\Words.txt");
                 int Count = 0;
                 Count = words.Count;
                 foreach (string line in words)
-                {
+                {  // Increase the count so that it can go to the next word.
                     Count++;
                 }
                 Console.WriteLine($"Successfully imported {words.Count} words from file.");
@@ -95,13 +114,13 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This method Sorts the words on the Imported file in the Bubblesort style and displays the compu time in mills.
         static void BubbleSortWords()
         {
-            try
-            {
+            try { 
+            // Bubble sorts
                 List<string> sortedWords = new List<string>(Words);
-
+                // begin the time recorded for sorting time.
                 DateTime startTime = DateTime.Now;
 
                 for (int i = 0; i < sortedWords.Count - 1; i++)
@@ -116,7 +135,7 @@ namespace Lab1
                         }
                     }
                 }
-
+                // Closes the time record for sorting time.
                 DateTime endTime = DateTime.Now;
                 TimeSpan duration = endTime - startTime;
 
@@ -127,7 +146,7 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method sorts the words on the Imported file in the lamda sort style and outputs the compu tim in mills.
         static void LINQSortWords()
         {
             try
@@ -135,7 +154,7 @@ namespace Lab1
                 List<string> sortedWords = new List<string>(words);
 
                 DateTime startTime = DateTime.Now;
-
+                // Lamda  doing it's thing for very very quick sorting.
                 sortedWords = sortedWords.OrderBy(w => w).ToList();
 
                 DateTime endTime = DateTime.Now;
@@ -146,11 +165,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method counts up the distinct words with in the file and outputs the total.
         static void CountDistinctWords()
         {
             try
-            {
+            {   // Simple count ofthe words which are distinct.
                 int distinctCount = words.Distinct().Count();
                 Console.WriteLine($"Distinct words count: {distinctCount}");
             }
@@ -159,11 +178,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method Displays the last 50 words with in the file.
         static void TakeLast50Words()
         {
-            try
-            {
+            try { 
+                // targetting the last 50 words for displaying.
                 List<string> last50Words = words.TakeLast(50).ToList();
                 Console.WriteLine("Last 50 words:");
                 foreach (string word in last50Words)
@@ -176,11 +195,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method displays the whole list of words from the file in reverse order.
         static void ReversePrintWords()
         {
             try
-            {
+            {   // Taking the order of the list and then Reversing the order and print it out.
                 List<string> reversedWords = words.Reverse().ToList();
                 Console.WriteLine("Words in reverse order:");
                 foreach (string word in reversedWords)
@@ -193,11 +212,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method finds the words ending with the letter D and then displays them.
         static void GetWordsEndingWithD()
         {
             try
-            {
+            {   // quick serah for words ending with d.
                 List<string> wordsEndingWithD = words.Where(w => w.EndsWith("d")).ToList();
                 Console.WriteLine($"Words ending with 'd' ({wordsEndingWithD.Count}):");
                 foreach (string word in wordsEndingWithD)
@@ -210,11 +229,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method finds the words starting with the letter R and then displays them.
         static void GetWordsStartingWithR()
         {
             try
-            {
+            {   // Quick seach for words starting with  'r' and displaying them.
                 List<string> wordsStartingWithR = words.Where(w => w.StartsWith("r")).ToList();
                 Console.WriteLine($"Words starting with 'r' ({wordsStartingWithR.Count}):");
                 foreach (string word in wordsStartingWithR)
@@ -227,11 +246,11 @@ namespace Lab1
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+        // This Method finds the words Starting with the letter A and which are also longer then Three letters and displays them.
         static void GetWordsLongerThan3AndStartsWithA()
         {
             try
-            {
+            {   // Seachs the file for words the start with the letter 'a' && that are more then 3 letters long.
                 List<string> wordsLongerThan3AndStartsWithA = words.Where(w => w.Length > 3 && w.StartsWith("a")).ToList();
                 Console.WriteLine($"Words longer than 3 characters and starting with 'a' ({wordsLongerThan3AndStartsWithA.Count}):");
                 foreach (string word in wordsLongerThan3AndStartsWithA)
